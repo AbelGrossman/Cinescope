bf9cece692e8e9c8716a15464e9048ea471c5667
Abel <abel.grossman@etu.univ-paris1.fr>
2025-01-27 16:08:26 +0100

    feat(main): created main components

---
+++ Cinescope/src/app/components/movie-ratings/movie-ratings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-ratings',
+  standalone: true,
+  imports: [],
+  templateUrl: './movie-ratings.component.html',
+  styleUrl: './movie-ratings.component.scss'
+})
+export class MovieRatingsComponent {
+
+}
---
+++ Cinescope/src/app/components/user-ratings/user-ratings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-ratings',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-ratings.component.html',
+  styleUrl: './user-ratings.component.scss'
+})
+export class UserRatingsComponent {
+
+}
